b84de53542931bb4c3f22c7ad4dc1a8e8c1c17f3
Allen Liu <nan6676@gmail.com>
2019-09-29 23:04:03 +0800

    update0929-1

--- app.py
+++ app.py
@@ -18,10 +18,6 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('8NDvVLUVZqlsmuVRXT0BcD2Qv8CDCXfCF/JCnsw7sla2ZV/HzgdYiMxJIjNKbEChLivFSlzZVmEVzGqmERk1sMcBoIqBqrrTQ35+PkQYJcKBSXoerddVUNcseYxBVGFSq8RD6dEtGwSl23mmr/r7eQdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('a5ccb4720386225cccbe5f66d1c9978d')
 
-# Your Account SID from twilio.com/console
-account_sid = "ACbb17bc67f6247ac1cf987d433e8e15fd"
-# Your Auth Token from twilio.com/console
-auth_token  = "2a30547eb0c7eb73b023d44828e9607e"
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -55,9 +51,13 @@ def handle_message(event):
 if __name__ == "__main__":
     app.run()
 
+# Your Account SID from twilio.com/console
+account_sid = "ACbb17bc67f6247ac1cf987d433e8e15fd"
+# Your Auth Token from twilio.com/console
+auth_token  = "2a30547eb0c7eb73b023d44828e9607e"
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
     to="+886939525301", 
     from_="+18024414035",
-    body= r)
+    body="YES,今天好天氣*3")
