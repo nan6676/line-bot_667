89ff0ffc89120ec0880b8134a607c6530904f321
Allen Liu <nan6676@gmail.com>
2019-10-02 22:05:27 +0800

    update

--- app.py
+++ app.py
@@ -51,13 +51,3 @@ def handle_message(event):
 if __name__ == "__main__":
     app.run()
 
-# Your Account SID from twilio.com/console
-account_sid = "ACbb17bc67f6247ac1cf987d433e8e15fd"
-# Your Auth Token from twilio.com/console
-auth_token  = "2a30547eb0c7eb73b023d44828e9607e"
-client = Client(account_sid, auth_token)
-
-message = client.messages.create(
-    to="+886939525301", 
-    from_="+18024414035",
-    body="YES,今天好天氣*3")
