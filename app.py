5ca25bd0958f84f5bdd926fad9e9a1b3668bbf6a
Allen Liu <nan6676@gmail.com>
2019-10-14 23:51:52 +0800

    update

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ def callback():
 def handle_message(event):
     mag = event.message.text
     r = event.message.text
+    r.split(str = "")
     str(r)[::-1]
     line_bot_api.reply_message(
         event.reply_token,  
