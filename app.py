564f1d777ce7b1e26b7289116938b9da4ec3d9b4
nan6676 <nan6676@gmail.com>
2020-06-09 22:34:49 +0800

    update

--- app.py
+++ app.py
@@ -13,6 +13,14 @@ from linebot.models import (
 
 from twilio.rest import Client
 
+#import requests
+from bs4 import BeautifulSoup
+
+import json
+
+import pandas as pd
+
+from datetime import datetime,timezone,timedelta
 
 app = Flask(__name__)
 
@@ -59,6 +67,67 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=f'美元 USD 對台幣 TWD：1:{usd_to_twd}'))
+    elif input_text == '@回家':
+        dt1 = datetime.utcnow().replace(tzinfo=timezone.utc)
+        dt2 = dt1.astimezone(timezone(timedelta(hours=8)))
+        now_day = dt2.strftime("%Y/%m/%d")
+        now_time = dt2.strftime("%H:%M")
+        url = 'http://www.thsrc.com.tw/tw/TimeTable/Search'
+        form_data = {
+        'StartStation':'2f940836-cedc-41ef-8e28-c2336ac8fe68',
+        'StartStationName': '南港站',
+        'EndStation':'fbd828d8-b1da-4b06-a3bd-680cdca4d2cd',
+        'EndStationName': '桃園站',
+        'DepartueSearchDate': now_day,
+        'DepartueSearchTime': now_time,
+        'SearchType':'S'    
+        }
+        # 用request.post，並放入form_data
+        
+        response_post = requests.post(url, data=form_data)
+        response_post.text
+
+        # 用json解析, 並分析資料結構
+        data = json.loads(response_post.text)
+        #data
+
+
+        trainItem = data['data']['DepartureTable']['TrainItem']
+
+        # 所有班車(train_number)
+        train_numbers = []
+        for item in trainItem:
+            train_numbers.append(item['TrainNumber'])
+
+        # 所有出發時間(departure_time)
+        departure_times = []
+        for item in trainItem:
+            departure_times.append(item['DepartureTime'])
+
+        # 所有到達時間(arrival_time)
+        arrival_times = []
+        for item in trainItem:
+            arrival_times.append(item['DestinationTime'])
+
+        # 所有行車時間(duration)
+        duration = []
+        for item in trainItem:
+            duration.append(item['Duration'])
+
+        # 整理成表格
+        highway_df = pd.DataFrame({
+            '車次': train_numbers,
+            '出發時間': departure_times,
+            '到達時間': arrival_times,
+            '行車時間': duration},
+            columns = ['車次', '出發時間', '到達時間', '行車時間'])
+
+
+
+
+            
+
+            
 
 
 if __name__ == "__main__":
