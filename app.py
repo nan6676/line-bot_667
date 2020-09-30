6b6808b73796a45e94c8dca0f824d60e12b6e6fc
nan6676 <nan6676@gmail.com>
2020-09-30 23:22:31 +0800

    update

--- app.py
+++ app.py
@@ -185,7 +185,7 @@ def handle_message(event):
 
         line_bot_api.reply_message(
                     event.reply_token,
-                    TextSendMessage(text= f'{ line}'))
+                    TextSendMessage(text= f'{ mobile01}'))
                 
 
 
