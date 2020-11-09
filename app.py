c9c1e6f7269dd54d8573d5fab41f59a7877c750b
nan6676 <nan6676@gmail.com>
2020-11-09 21:27:50 +0800

    update

--- app.py
+++ app.py
@@ -41,11 +41,11 @@ except LineBotApiError as e:
      error handle
     ...'''
 # 增加的這段放在下面
-'''
+
 @app.route("/")
 def home():
     return render_template("home.html")
-'''
+
 # 接收 LINE 的資訊
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -227,7 +227,7 @@ def handle_message(event):
             event.reply_token,
             TextSendMessage(text='好圖'))
         ext = 'jpg'
-        message_content = line_bot_api.get_message_content(event.message.id)
+        message_content = line_bot_api.get_message_content('<message_id>')#(event.message.id)
         with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=ext + '-', delete=False) as tf:
             for chunk in message_content.iter_content():
                 tf.write(chunk)
