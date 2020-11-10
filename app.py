552d47bbaa060679754f86d857a08b33dc4bd46b
nan6676 <nan6676@gmail.com>
2020-11-10 23:02:00 +0800

    update

--- app.py
+++ app.py
@@ -66,156 +66,157 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=(TextMessage, ImageMessage))
 def handle_message(event):
+    if isinstance(event.message, TextMessage):
 
-    # get user id when reply
-    user_id = event.source.user_id
-    #print("user_id =", user_id)
-    #mag = event.message.text
-    #r = event.message.text
-    #s = str(mag)[::-1]
-    #r = s
-    #r.split(r = "")
-    #str(r)[::-1]
-    #line_bot_api.reply_message(
-        #event.reply_token,  
-        #TextSendMessage(text=r))
-    input_text = event.message.text
-    if input_text == '@查詢匯率':
-        resp = requests.get('https://tw.rter.info/capi.php')
-        currency_data = resp.json()
-        usd_to_twd = currency_data['USDTWD']['Exrate']
+        # get user id when reply
+        user_id = event.source.user_id
+        #print("user_id =", user_id)
+        #mag = event.message.text
+        #r = event.message.text
+        #s = str(mag)[::-1]
+        #r = s
+        #r.split(r = "")
+        #str(r)[::-1]
+        #line_bot_api.reply_message(
+            #event.reply_token,  
+            #TextSendMessage(text=r))
+        input_text = event.message.text
+        if input_text == '@查詢匯率':
+            resp = requests.get('https://tw.rter.info/capi.php')
+            currency_data = resp.json()
+            usd_to_twd = currency_data['USDTWD']['Exrate']
 
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=f'美元 USD 對台幣 TWD：1:{usd_to_twd}'))
-    elif input_text == '@回家':
-        dt1 = datetime.utcnow().replace(tzinfo=timezone.utc)
-        dt2 = dt1.astimezone(timezone(timedelta(hours=8)))
-        now_day = dt2.strftime("%Y/%m/%d")
-        now_time = dt2.strftime("%H:%M")
-        url = 'https://www.thsrc.com.tw/TimeTable/Search'
-        form_data = {
-        'SearchType':'S',
-        'Lang': 'TW',
-        'StartStation': 'NanGang',
-        'EndStation': 'TaoYuan',
-        'OutWardSearchDate': now_day,
-        'OutWardSearchTime': now_time,
-        'ReturnSearchDate': now_day,
-        'ReturnSearchTime': now_time    
-        }
-        # 用request.post，並放入form_data
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=f'美元 USD 對台幣 TWD：1:{usd_to_twd}'))
+        elif input_text == '@回家':
+            dt1 = datetime.utcnow().replace(tzinfo=timezone.utc)
+            dt2 = dt1.astimezone(timezone(timedelta(hours=8)))
+            now_day = dt2.strftime("%Y/%m/%d")
+            now_time = dt2.strftime("%H:%M")
+            url = 'https://www.thsrc.com.tw/TimeTable/Search'
+            form_data = {
+            'SearchType':'S',
+            'Lang': 'TW',
+            'StartStation': 'NanGang',
+            'EndStation': 'TaoYuan',
+            'OutWardSearchDate': now_day,
+            'OutWardSearchTime': now_time,
+            'ReturnSearchDate': now_day,
+            'ReturnSearchTime': now_time    
+            }
+            # 用request.post，並放入form_data
         
-        response_post = requests.post(url, data=form_data)
-        response_post.text
+            response_post = requests.post(url, data=form_data)
+            response_post.text
 
-        # 用json解析, 並分析資料結構
-        data = json.loads(response_post.text)
-        #data
+            # 用json解析, 並分析資料結構
+            data = json.loads(response_post.text)
+            #data
 
 
-        trainItem = data['data']['DepartureTable']['TrainItem']
+            trainItem = data['data']['DepartureTable']['TrainItem']
 
-        # 所有班車(train_number)
-        train_numbers = []
-        for item in trainItem:
-            train_numbers.append(item['TrainNumber'])
+            # 所有班車(train_number)
+            train_numbers = []
+            for item in trainItem:
+                train_numbers.append(item['TrainNumber'])
 
-        # 所有出發時間(departure_time)
-        departure_times = []
-        for item in trainItem:
-            departure_times.append(item['DepartureTime'])
+            # 所有出發時間(departure_time)
+            departure_times = []
+            for item in trainItem:
+                departure_times.append(item['DepartureTime'])
             
-        # 所有到達時間(arrival_time)
-        arrival_times = []
-        for item in trainItem:
-            arrival_times.append(item['DestinationTime'])
+            # 所有到達時間(arrival_time)
+            arrival_times = []
+            for item in trainItem:
+                arrival_times.append(item['DestinationTime'])
 
-        # 所有行車時間(duration)
-        duration = []
-        for item in trainItem:
-            duration.append(item['Duration'])
+            # 所有行車時間(duration)
+            duration = []
+            for item in trainItem:
+                duration.append(item['Duration'])
 
         # 自由坐車廂(NonReservedCar)
-        nonreservedcar = []
-        for item in trainItem:
-            nonreservedcar.append(item['NonReservedCar'])            
+            nonreservedcar = []
+            for item in trainItem:
+                nonreservedcar.append(item['NonReservedCar'])            
 
         # 整理成表格
-        highway_df = pd.DataFrame({
-            '車次': train_numbers,
-            '從南港': departure_times,
-            '到桃園': arrival_times,
-            '行車時間': duration,
-            '自由坐車廂': nonreservedcar},
-            columns = [ '從南港', '到桃園', '行車時間', '自由坐車廂'], index = train_numbers)
-        filter = highway_df["從南港"] > now_time
-        highway_df1 = highway_df[filter]
+            highway_df = pd.DataFrame({
+                '車次': train_numbers,
+                '從南港': departure_times,
+                '到桃園': arrival_times,
+                '行車時間': duration,
+                '自由坐車廂': nonreservedcar},
+                columns = [ '從南港', '到桃園', '行車時間', '自由坐車廂'], index = train_numbers)
+            filter = highway_df["從南港"] > now_time
+            highway_df1 = highway_df[filter]
 
 
 
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=f'{highway_df1}')) 
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=f'{highway_df1}')) 
 
-    elif input_text == '@ID':   
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=f'{user_id}'))#(text=f'{user_id}')
+        elif input_text == '@ID':   
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=f'{user_id}'))#(text=f'{user_id}')
 
-    elif input_text == '@話題':
-        web = 'https://www.mobile01.com/hottopics.php'
-        res = requests.get(web, headers={'User-Agent': 'Custom'})
-        soup = BeautifulSoup(res.text, 'html.parser')
-        articles = soup.select('.c-listTableTd__title a')
-        issue = []
-        form = []
-        forum = []
-        line = 'Mobile01熱門討論'+'\n'
+        elif input_text == '@話題':
+            web = 'https://www.mobile01.com/hottopics.php'
+            res = requests.get(web, headers={'User-Agent': 'Custom'})
+            soup = BeautifulSoup(res.text, 'html.parser')
+            articles = soup.select('.c-listTableTd__title a')
+            issue = []
+            form = []
+            forum = []
+            line = 'Mobile01熱門討論'+'\n'
         
-        for each_title in articles:
-            if 'title' in (str(each_title)):
-                issue.append(each_title.text)
-                form.append('https://www.mobile01.com/'+ each_title['href'])
-            elif 'span' in (str(each_title)):
-                forum.append(each_title.text)
+            for each_title in articles:
+                if 'title' in (str(each_title)):
+                    issue.append(each_title.text)
+                    form.append('https://www.mobile01.com/'+ each_title['href'])
+                elif 'span' in (str(each_title)):
+                    forum.append(each_title.text)
 
-        count = (len(issue))
-        for x in range(count):
-            line = (str(line))+ ((str(x))+'.■ '+ issue[x]+' '+ forum[x]+ ' ' + form[x] +'\n')
+            count = (len(issue))
+            for x in range(count):
+                line = (str(line))+ ((str(x))+'.■ '+ issue[x]+' '+ forum[x]+ ' ' + form[x] +'\n')
         
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text= f'{ line}'))
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text= f'{ line}'))
 
     
-    elif input_text == '@ptt':
-        web = 'https://disp.cc/b/PttHot'
-        res = requests.get(web)
-        soup = BeautifulSoup(res.text, 'html.parser')
-        articles = soup.select('.row2 a')
+        elif input_text == '@ptt':
+            web = 'https://disp.cc/b/PttHot'
+            res = requests.get(web)
+            soup = BeautifulSoup(res.text, 'html.parser')
+            articles = soup.select('.row2 a')
 
-        issue = []
-        form = []
-        forum = []
-        line = 'PTT熱門討論'+'\n'
+            issue = []
+            form = []
+            forum = []
+            line = 'PTT熱門討論'+'\n'
 
-        for each_title in articles:
-            if 'titleColor' in (str(each_title)):
-                issue.append(each_title.text)
-                form.append('https://disp.cc/b/'+ each_title['href'])
-            elif 'target' in (str(each_title)):
-                forum.append(each_title.text)
+            for each_title in articles:
+                if 'titleColor' in (str(each_title)):
+                    issue.append(each_title.text)
+                    form.append('https://disp.cc/b/'+ each_title['href'])
+                elif 'target' in (str(each_title)):
+                    forum.append(each_title.text)
         
-        count = (len(issue))
-        for x in range(count):
-            line = (str(line))+ ((str(x))+'.'+ issue[x]+' '+ form[x] +'\n')
+            count = (len(issue))
+            for x in range(count):
+                line = (str(line))+ ((str(x))+'.'+ issue[x]+' '+ form[x] +'\n')
 
-        line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text= f'{ line}'))
+            line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text= f'{ line}'))
                 
 
 '''
