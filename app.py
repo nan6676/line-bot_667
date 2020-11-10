7bc3acd6e3d7c85344cefa15c26c2a5383ae0433
nan6676 <nan6676@gmail.com>
2020-11-10 22:04:07 +0800

    update

--- app.py
+++ app.py
@@ -229,10 +229,10 @@ def handle_content_message(event):
         ext = 'jpg'
         #static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
         message_content = line_bot_api.get_message_content(event.message.id)#(event.message.id)
-        with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=ext + '-') as tf:
+        with open("./images/", 'wb') as fd:
             for chunk in message_content.iter_content():
-                tf.write(chunk)
-            tempfile_path = tf.name
+                fd.write(chunk)
+            tempfile_path = fd.name
 
 
             dist_path = tempfile_path + '.' + ext
