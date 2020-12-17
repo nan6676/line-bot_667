7a86daf0ac993b7360f7ae2eb381ef46e67ed11f
nan6676 <nan6676@gmail.com>
2020-12-17 23:04:33 +0800

    update

--- config.py
+++ config.py
@@ -9,16 +9,16 @@ refresh_token = 'd4a1fd24541add0b69475d718fa428e53c7c06c9'
 line_channel_access_token = '8NDvVLUVZqlsmuVRXT0BcD2Qv8CDCXfCF/JCnsw7sla2ZV/HzgdYiMxJIjNKbEChLivFSlzZVmEVzGqmERk1sMcBoIqBqrrTQ35+PkQYJcKBSXoerddVUNcseYxBVGFSq8RD6dEtGwSl23mmr/r7eQdB04t89/1O/w1cDnyilFU='
 line_channel_secret = 'a5ccb4720386225cccbe5f66d1c9978d'
 uesr_name_myself = {
-'U52dfc11c1bfc683323591e9093c16b6d':'永慶',
-'Uf3195a3c5cc538e9e58d1f0031091bba':'建均',
-'Uc83e9226ace83e452c8fe5ee79ed8ed3':'威頤',
-'U466edffe1cbbc77a9efde0c612ea34a0':'建博',
-'U9f1352695f6b209a738c248087843202':'鈺鏵',
-'Uafda8f7a900d2a3ac5e0f203f2f9d14a':'俊賢',
-'U7262f5fc768887475f2a9baf97d23b1e':'俊軒',
+'U52dfc11c1bfc683323591e9093c16b6d':'慶哥',
+'Uf3195a3c5cc538e9e58d1f0031091bba':'均哥',
+'Uc83e9226ace83e452c8fe5ee79ed8ed3':'威哥',
+'U466edffe1cbbc77a9efde0c612ea34a0':'博哥',
+'U9f1352695f6b209a738c248087843202':'鈺哥',
+'Uafda8f7a900d2a3ac5e0f203f2f9d14a':'賢哥',
+'U7262f5fc768887475f2a9baf97d23b1e':'軒哥',
 'Ud29be03aef3ec3478d8a8366db7fc9de':'Carlos Huang',
 'Uc607ab2ccc4ac029f44b743c7b1338bc':'勇男',
-'U111817bb3803a6414e1aa7ae9a181bd3':'展',
-'U636ee51de9f33915584a7c55a2a63840':'黃聖滑倒又爬起來',
-'U1fdb9285e295e324ba2d572c93f8b3bb':'奇怪的哥哥',
+'U111817bb3803a6414e1aa7ae9a181bd3':'展哥',
+'U636ee51de9f33915584a7c55a2a63840':'滑倒又爬起來流出黃黃的東西',
+'U1fdb9285e295e324ba2d572c93f8b3bb':'奇怪哥哥',
 }
